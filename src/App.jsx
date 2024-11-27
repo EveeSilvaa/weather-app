3aa25a158ca45217b908b30c0e8908b7f34301aa
Evellyn Silva <evellynmaria593@gmail.com>
2024-11-27 10:55:38 -0300

    tentando ajeitar o problemakkkk <3

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
 import CurrentWeather from "./components/CurrentWeather";
-import HourlyWeatherItem from "./components/HourlyWeatherItem";
 import SearchSection from "./components/SearchSection";
+import HourlyWeatherItem from "./components/HourlyWeatherItem";
+import { weatherCodes } from "./constants";
 
 const App = () => {
   const [CurrentWeather, setCurrentWeather] = useState({});
@@ -12,40 +13,36 @@ const App = () => {
       const response = await fetch(API_URL);
       const data = await response.json();
       
-      const temperature = data.current.temp_c;
+      // extract current weather data
+      const temperature = Math.floor(data.current.temp_c);
       const description = data.current.condition.text;
+      const weatherIcon = Object.keys(weatherCodes).find(icon => weatherCodes[icon].includes(data.current.condition.code));
 
-      setCurrentWeather({temperature, description});
+      setCurrentWeather({temperature, description, weatherIcon});
     } catch (error) {
       console.log(error);
     }
   };
 
 
-  return <div className="container">
-   {/* search section */}
-   <SearchSection getWeatherDeatails={getWeatherDeatails}/>
-
+  return (
+    <div className="container">
+     {/* search section */}
+    <SearchSection getWeatherDeatails={getWeatherDeatails}/>
 
     {/* search section */}
     <div className="weather-section">
-      <CurrentWeather/>
+      <CurrentWeather currentWeather={ CurrentWeather }/>
 
-      {/* hourly weather forecast list */}
+    {/* hourly weather forecast list */}
       <div className="hourly-forecast">
         <ul className="weather-list">
-         <HourlyWeatherItem/>
-         <HourlyWeatherItem/>
-         <HourlyWeatherItem/>
-         <HourlyWeatherItem/>
-         <HourlyWeatherItem/>
-         <HourlyWeatherItem/>
-         <HourlyWeatherItem/>
          <HourlyWeatherItem/>
         </ul>
       </div>
     </div>
-  </div>;
-};
+  </div>
+  );
+}
 
 export default App;
